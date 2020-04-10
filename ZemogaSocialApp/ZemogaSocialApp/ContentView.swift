6d0d02a67b66987f4b7aa310f2cf508fccad47ec
Luis Alejandro Ramirez <lramirezsuarez@gmail.com>
2020-04-10 10:54:10 -0500

    Project creation and pod installation.

---
+++ ZemogaSocialApp/ZemogaSocialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZemogaSocialApp
+//
+//  Created by Luis Ramirez on 10/04/20.
+//  Copyright © 2020 Lramirez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
